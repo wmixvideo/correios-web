dd06fc161773592539d4f8c07c5633f5ad44b0d3
Diego Fincatto <diego@fincatto.com>
2017-05-13 13:41:19 -0300

    Criado home page para facilitar o mapeamento dos servicos.

--- src/main/java/com/github/wmixvideo/correiosweb/controllers/CorreiosRastreamentoRestController.java
+++ src/main/java/com/github/wmixvideo/correiosweb/controllers/CorreiosRastreamentoRestController.java
@@ -4,13 +4,13 @@ import br.com.correios.webservice.rastro.Sroxml;
 import com.github.wmixvideo.correios.WSCorreiosRastreador;
 import com.github.wmixvideo.correiosweb.CorreiosWebConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rastrear/{objeto}")
 public class CorreiosRastreamentoRestController {
 
     private final WSCorreiosRastreador correiosRastreador;
@@ -20,8 +20,8 @@ public class CorreiosRastreamentoRestController {
         correiosRastreador = new WSCorreiosRastreador(config.getUsuario(), config.getSenha());
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    Sroxml rastrear(@PathVariable String objeto) {
+    @RequestMapping(value = "/rastrear/{objeto}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    Sroxml rastrearObjeto(@PathVariable String objeto) {
         return correiosRastreador.consultaObjeto(objeto);
     }
 }
